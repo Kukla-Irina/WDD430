51e897bc94ad35bd978adb40d29ce816d3c509e9
Kukla-Irina <kuk18002@byui.edu>
2025-04-09 23:57:36 +0500

    Final version

--- final/task-tracker/frontend/src/app/components/task-list/task-list.component.ts
+++ final/task-tracker/frontend/src/app/components/task-list/task-list.component.ts
@@ -1,20 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Task } from '../../models/task.model';
 import { TaskService } from '../../services/task.service';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-task-list',
   standalone: false,
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.css'
+  styleUrls: ['./task-list.component.css']
 })
-export class TaskListComponent implements OnInit {
+export class TaskListComponent implements OnInit, OnDestroy {
   tasks: Task[] = [];
+  private pollSubscription: Subscription;
 
   constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
-    this.fetchTasks();
+    this.fetchTasks(); 
+    this.startPolling(); 
+  }
+
+  ngOnDestroy(): void {
+    if (this.pollSubscription) {
+      this.pollSubscription.unsubscribe(); 
+    }
   }
 
   fetchTasks(): void {
@@ -30,5 +39,11 @@ export class TaskListComponent implements OnInit {
   toggleComplete(task: Task): void {
     task.completed = !task.completed;
     this.taskService.updateTask(task).subscribe();
+  }
+
+  startPolling(): void {
+    this.pollSubscription = interval(2000).subscribe(() => {
+      this.fetchTasks(); 
+    });
   }
 }
